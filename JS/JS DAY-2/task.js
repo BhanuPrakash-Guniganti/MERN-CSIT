9ffcf5e7aae9fa4fbac5e0aa17a648f3e20b60fd
Bhanu Prakash <gunigantibhanu@gmail.com>
2026-07-31 15:45:28 +0530

    JS Loops

---
+++ JS/JS DAY-2/task.js
@@ -0,0 +1,50 @@
+/*
+// print 1-10 numbers 
+for (i = 0; i<10;i++){
+    console.log(i);
+}
+*/
+
+
+
+/*
+// print even numbers between 0-10
+for (i = 0; i < 10; i++) {
+    if(i % 2 == 0) {
+        console.log("even numbers: " + i);
+    }
+}
+*/
+
+
+
+/*
+// sum of first 10 numbers
+let sum = 0;
+for (i = 0; i < 10; i++) {
+    sum += i;
+}
+console.log("Sum: " + sum);
+*/
+
+
+/*
+//length of numbers
+let num = 123455;
+let length = num.toString().length;
+console.log(length);
+*/
+
+
+/*
+//length of numbers another way
+ n = 153;
+ sum = 0;
+while (n > 0) {
+    var digit = n % 10;
+    sum += digit ** 3;
+    n = Math.floor(n / 10);
+}
+console.log(sum);
+*/
+
