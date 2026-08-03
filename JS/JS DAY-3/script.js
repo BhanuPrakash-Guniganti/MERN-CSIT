f82d07de507f13a1f7a23d13f97e1f55b0d0e1c6
Bhanu Prakash <gunigantibhanu@gmail.com>
2026-08-03 12:53:12 +0530

    morning session array, str

---
+++ JS/JS DAY-3/script.js
@@ -0,0 +1,13 @@
+// Creating Student Data sem wise
+let student = "Raj"
+let sem1Marks = 95;
+let sem2Marks = 88;
+let sem3Marks = 85;
+let sem4Marks = 92;
+
+console.log("Student Name: " + student);
+console.log("Sem 1 Marks: " + sem1Marks);
+console.log("Sem 2 Marks: " + sem2Marks);
+console.log("Sem 3 Marks: " + sem3Marks);
+console.log("Sem 4 Marks: " + sem4Marks);
+
